be715a71a648c1aaa204457fe271005ec3c6c8a8
hardy2p <pushpendrasinghshekhawat500@gmail.com>
2025-01-10 23:37:12 +0530

    New commits

---
+++ Basics_02/11_objects2.js
@@ -0,0 +1,89 @@
+//Singelton objects are objects that are created only once and are shared by all the modules that require them.
+//In Node.js, module caching allows us to create a singleton object.
+//Singelton objects are always created using the class constructor.
+//The object decalred normally are known as object literal not the singelton objects.
+
+const studentData=new Object();
+
+studentData.id="20BCS4137";
+studentData.name="Pushpendra"; 
+studentData.isLoggedin=false;
+// console.log(studentData);
+
+
+//Nested Objects 
+
+const student={
+    id:"20BCS4137",
+    name:{
+        userFullName:{
+            firstName:"Pushpendra",
+            lastName:"Singh"
+        }
+    },
+    isLoggedin:false,
+    address:{
+        city:"Delhi",
+        state:"Delhi",
+        country:"India"
+    }
+}
+
+console.log(student.name.userFullName.firstName);
+
+
+//Addding objects together
+const obj1 = {
+    key1: "value1",
+    key2: "value2"
+};
+
+const obj2 = {
+    keyA: "valueA",
+    keyB: "valueB"
+};
+
+const obj3={...obj1,...obj2};//Using spread operator we can add two objects just like in arraus 
+console.log(obj3);
+// Creating two new object literals and merging them
+const obj4 = {
+    keyX: "valueX",
+    keyY: "valueY"
+};
+
+const obj5 = {
+    keyM: "valueM",
+    keyN: "valueN"
+};
+
+const obj6 = Object.assign({},obj4,obj5);
+//All the obj4 and 5 are getting copied in {} object
+
+console.log(obj6);
+
+
+// Array of objects named users
+const users = [
+    {
+        id: "1",
+        name: "Alice",
+        email: "alice@example.com"
+    },
+    {
+        id: "2",
+        name: "Bob",
+        email: "bob@example.com"
+    },
+    {
+        id: "3",
+        name: "Charlie",
+        email: "charlie@example.com"
+    }
+];
+
+console.log(users);
+
+
+//extract keys and values
+console.log(Object.keys(obj6));//Return all the keys in form of an array.
+console.log(Object.values(obj6));//Returns all the values in form of an array.
--- Basics_02/tempCodeRunnerFile.js
+++ Basics_02/tempCodeRunnerFile.js
@@ -1,20 +1,63 @@
-const mySym = Symbol("symBol Key");
+//Singelton objects are objects that are created only once and are shared by all the modules that require them.
+// //In Node.js, module caching allows us to create a singleton object.
+// //Singelton objects are always created using the class constructor.
+// //The object decalred normally are known as object literal not the singelton objects.
 
-const myUser={
-    name:"Pushpendra",
-    "age":23,
-    email:"pushpa@gmail.com",
-    loggedIn:false,
-    lastLoggedIn:["Monday","Tuesday"],
-    [mySym]: "symbol key"
-};
+// const studentData=new Object();
 
-// console.log(myUser[mySym]);
-// console.log(myUser.name);
-// console.log(myUser.email);
-// console.log(myUser["age"]);
-// console.log(myUser.age);
-// console.log(myUser["loggedIn"]);
+// studentData.id="20BCS4137";
+// studentData.name="Pushpendra"; 
+// studentData.isLoggedin=false;
+// // console.log(studentData);
 
-myUser.email="newEmail@gmail.com";
-// console.log(myUser.email);
+
+// //Nested Objects 
+
+// const student={
+//     id:"20BCS4137",
+//     name:{
+//         userFullName:{
+//             firstName:"Pushpendra",
+//             lastName:"Singh"
+//         }
+//     },
+//     isLoggedin:false,
+//     address:{
+//         city:"Delhi",
+//         state:"Delhi",
+//         country:"India"
+//     }
+// }
+
+// console.log(student.name.userFullName.firstName);
+
+
+// //Addding objects together
+// const obj1 = {
+//     key1: "value1",
+//     key2: "value2"
+// };
+
+// const obj2 = {
+//     keyA: "valueA",
+//     keyB: "valueB"
+// };
+
+// const obj3={...obj1,...obj2};//Using spread operator we can add two objects just like in arraus 
+// console.log(obj3);
+// // Creating two new object literals and merging them
+// const obj4 = {
+//     keyX: "valueX",
+//     keyY: "valueY"
+// };
+
+// const obj5 = {
+//     keyM: "valueM",
+//     keyN: "valueN"
+// };
+
+// const obj6 = Object.assign({},obj4,obj5);
+// //All the obj4 and 5 are getting copied in {} object
+
+// console.log(obj6);
+
