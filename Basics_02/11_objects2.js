c68630fcbaeeeea5d806d352f5d46a1bbfd143bc
hardy2p <pushpendrasinghshekhawat500@gmail.com>
2025-01-11 11:08:01 +0530

    Good morning  commit

--- Basics_02/11_objects2.js
+++ Basics_02/11_objects2.js
@@ -87,3 +87,5 @@ console.log(users);
 //extract keys and values
 console.log(Object.keys(obj6));//Return all the keys in form of an array.
 console.log(Object.values(obj6));//Returns all the values in form of an array.
+console.log(Object.entries(obj6));//Returns an array of arrays with nested array of size 2 where 0th element would be key and 1st element would be value to that 0th key.
+console.log(obj6.hasOwnProperty('keyY'));
