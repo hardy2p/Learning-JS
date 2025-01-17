6529fc487a9ff2ef86a0ad6584f1adce8d4fd079
hardy2p <pushpendrasinghshekhawat500@gmail.com>
2025-01-17 16:50:22 +0530

    Added projects and done the first colot changer project

---
+++ 07_Projects/01_colorChanger/script.js
@@ -0,0 +1,9 @@
+const buttons=document.querySelectorAll('.button');
+const body=document.querySelector("body");
+
+buttons.forEach((btn)=>{
+  //console.log(btn);
+  btn.addEventListener('click',(e)=>{
+    body.style.backgroundColor=`${e.target.id}`;
+  })
+})
